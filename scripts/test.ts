6752046f2f82cb42e3f273a6836dbb05542b29f9
James Bond <james@protoss.exchange>
2023-04-08 22:10:13 +0900

    fix the test script

--- scripts/test.ts
+++ scripts/test.ts
@@ -2,6 +2,11 @@ import { number, Contract, Account, ec, Provider, SequencerProvider, stark, uint
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+// ClassHash
+const protossFactoryClassHash = "0x7b2dcbbf952e8668b4ab2afe40b99e6a023d5355f332f27644a0cac6537af48";
+const protossPairClassHash = "0x7f052fb14fc79a77eea424b14ac2126400bed7b4e79b6b01b4dbcf823ec15d0";
+const protossRouterClassHash = "0x4e30750c4e203e514b5910ad9702d1525f43a04bb03abef45c30fccf6a3437a";
+
 // Account
 const accountAddress = "0x076b885Ca6e863346774BEaF32f8752db6af9eEBEc2A9CB0D321725Cad6c197e";
 const privateKey = process.env.PROTOSTAR_ACCOUNT_PRIVATE_KEY || "";
@@ -17,15 +22,9 @@ const tokenDAddress = "0x55ce6e2c9f7e962ceddce755ac5dbed6415d206d5435631557d1d75
 const factoryAddress = "0x0017b7cdbd6dd86b4b9baf572e040e74bd44b419dcbb6f38a2e936aec9327b8e";
 
 // ProtossRouter
-const routerAddress = "0x005a25278b5b3d935d5871fe8f8cf824840dd7a9082dcbd5d37ab2e74747b2ed";
+const routerAddress = "0x02E071Eb551c4B084B751947aD172C3B210176eD2FC679C5e9C2Cc47Db91df2F";
 
-
-// const provider = new Provider({ sequencer: { network: 'goerli-alpha' } });
-const provider = new Provider({
-  rpc: {
-    nodeUrl: 'https://starknet-goerli.infura.io/v3/f33802aa97504278bf8ac3f6e85e3b7b',
-  },
-})
+const provider = new Provider({ sequencer: { network: 'goerli-alpha' } });
 
 const getContract = async (addr: string) => {
     const { abi: abi } = await provider.getClassAt(addr);
@@ -101,26 +100,25 @@ const createPair = async (tokenA: string, tokenB: string) => {
     showTxStatus(createPairTxHash);
 }
 
-// //// Solution 1 add liquidity
+//// Solution 1 add liquidity
 // const { transaction_hash: addLiquidityTxHash } = await router.addLiquidity(
 //   tokenA,
 //   tokenB,
-//   [1000000000, 0],
-//   [1000000000, 0],
+//   [amount, 0],
+//   [amount, 0],
 //   [0, 0],
 //   [0, 0],
 //   account.address,
-//   Math.floor((new Date().getTime() + 100000) / 1000)
+//   Math.floor((new Date().getTime() + 43200000) / 1000),
+//   { maxFee: res.suggestedMaxFee }
 // );
 // showTxStatus(addLiquidityTxHash);
-//
 const addLiquidity = async (tokenA: string, tokenB: string, amount: string) => {
     const router = await getContract(routerAddress);
     const account = getAccount();
     router.connect(account);
 
     // check if user has enough balance
-
     let res = await account.estimateInvokeFee({
       contractAddress: routerAddress,
       entrypoint: "addLiquidity",
@@ -132,23 +130,9 @@ const addLiquidity = async (tokenA: string, tokenB: string, amount: string) => {
         0, 0, // amountAMin
         0, 0, // amountBMin
         account.address,
-        Math.floor((new Date().getTime() + 10000) / 1000),
+        Math.floor((new Date().getTime() + 43200000) / 1000),
       ]
     });
-
-    //// Solution 1 add liquidity
-    // const { transaction_hash: addLiquidityTxHash } = await router.addLiquidity(
-    //   tokenA,
-    //   tokenB,
-    //   [amount, 0],
-    //   [amount, 0],
-    //   [0, 0],
-    //   [0, 0],
-    //   account.address,
-    //   Math.floor((new Date().getTime() + 100000) / 1000),
-    //   { maxFee: res.suggestedMaxFee }
-    // );
-    // showTxStatus(addLiquidityTxHash);
 
     const executeHash = await account.execute({
       contractAddress: routerAddress,
@@ -161,7 +145,7 @@ const addLiquidity = async (tokenA: string, tokenB: string, amount: string) => {
         '0', '0',
         '0', '0',
         account.address,
-        Math.floor((new Date().getTime() + 100000) / 1000),
+        Math.floor((new Date().getTime() + 43200000) / 1000),
       ]
     }, undefined, { maxFee: res.suggestedMaxFee });
     showTxStatus(executeHash.transaction_hash);
@@ -209,7 +193,7 @@ const removeLiquidity = async (tokenA: string, tokenB: string, amount: string) =
         0, 0, // amountAMin
         0, 0, // amountBMin
         account.address,
-        Math.floor((new Date().getTime() + 10000) / 1000),
+        Math.floor((new Date().getTime() + 43200000) / 1000),
       ]
     });
 
@@ -223,7 +207,7 @@ const removeLiquidity = async (tokenA: string, tokenB: string, amount: string) =
         0, 0, // amountAMin
         0, 0, // amountBMin
         account.address,
-        Math.floor((new Date().getTime() + 10000) / 1000),
+        Math.floor((new Date().getTime() + 43200000) / 1000),
       ]
     }, undefined, { maxFee: res.suggestedMaxFee });
     showTxStatus(executeHash.transaction_hash);
@@ -234,26 +218,8 @@ const approveLiquidityAllowance = async (tokenA: string, tokenB: string, amount:
     const account = getAccount();
     router.connect(account);
 
-    let res = await account.estimateInvokeFee({
-      contractAddress: routerAddress,
-      entrypoint: "approveLiquidityAllowance",
-      calldata: [
-        tokenA,
-        tokenB,
-        amount,
-      ]
-    });
-
-    const executeHash = await account.execute({
-      contractAddress: routerAddress,
-      entrypoint: "approveLiquidityAllowance",
-      calldata: [
-        tokenA,
-        tokenB,
-        amount,
-      ]
-    }, undefined, { maxFee: res.suggestedMaxFee });
-    showTxStatus(executeHash.transaction_hash);
+    const TokenATokenDPair = await getPairAddress(tokenAAddress, tokenDAddress);
+    approve(TokenATokenDPair, routerAddress, '100000000000000000000000');
 }
 
 const getPairContractAddress = async (tokenA: string, tokenB: string) => {
@@ -270,39 +236,88 @@ const showTxStatus = (txHash: string) => {
     console.log(`https://testnet.starkscan.co/tx/${txHash}`);
 }
 
-// //// Testing Call code
-// mintToken(tokenAAddress, '1000000000000000000000000');
-// mintToken(tokenBAddress, '1000000000000000000000000');
-// mintToken(tokenCAddress, '1000000000000000000000000');
-// mintToken(tokenDAddress, '1000000000000000000000000');
+const sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// createPair(tokenAAddress, tokenBAddress);
-// createPair(tokenAAddress, tokenCAddress);
-// createPair(tokenAAddress, tokenDAddress);
-// createPair(tokenBAddress, tokenCAddress);
-// createPair(tokenBAddress, tokenDAddress);
-// createPair(tokenCAddress, tokenDAddress);
+const checkAllowance = async (tokenAddress: string, spender: string) => {
+    const erc20 = await getContract(tokenAddress);
+    const account = getAccount();
+    erc20.connect(account);
 
-// getPairContractAddress(tokenAAddress, tokenBAddress);
-// getPairContractAddress(tokenAAddress, tokenCAddress);
-// getPairContractAddress(tokenAAddress, tokenDAddress);
-// getPairContractAddress(tokenBAddress, tokenCAddress);
-// getPairContractAddress(tokenBAddress, tokenDAddress);
-// getPairContractAddress(tokenCAddress, tokenDAddress);
+    const allowance = await erc20.allowance(account.address, spender);
+    console.log("Allowance =", allowance[0]);
+}
 
-// approve(tokenAAddress, routerAddress, uint256.UINT_128_MAX.toString());
-// approve(tokenBAddress, routerAddress, uint256.UINT_128_MAX.toString());
-// approve(tokenCAddress, routerAddress, uint256.UINT_128_MAX.toString());
-// approve(tokenDAddress, routerAddress, uint256.UINT_128_MAX.toString());
+const listAllPairs = async () => {
+    const factory = await getContract(factoryAddress);
+    const account = getAccount();
+    factory.connect(account);
 
-// addLiquidity(tokenAAddress, tokenBAddress, '50000000000000000000000');
-// addLiquidity(tokenAAddress, tokenCAddress, '50000000000000000000000');
-// addLiquidity(tokenAAddress, tokenDAddress, '50000000000000000000000');
-// addLiquidity(tokenBAddress, tokenCAddress, '50000000000000000000000');
-// addLiquidity(tokenBAddress, tokenDAddress, '50000000000000000000000');
-// addLiquidity(tokenCAddress, tokenDAddress, '50000000000000000000000');
+    const pairCount = await factory.allPairsLength();
+    console.log("Pair count =", number.toHexString(pairCount[0]));
 
-// approveLiquidityAllowance(tokenAAddress, tokenDAddress, '1000000_000000000000000000');
-// const TokenATokenDPair = "0x12028a058439d05115ef58d0885cf8d6a3223b5514ee60d3c87716dab5dab0c"
-// approve(TokenATokenDPair, routerAddress, '1000000_000000000000000000')
-// removeLiquidity(tokenAAddress, tokenDAddress, '1000000_000000000000000000');
+    for (let i = 0; i < pairCount[0]; i++) {
+      const pairAddress = await factory.allPairs(i);
+
+      const pair = await getContract(number.toHexString(pairAddress[0]));
+      const reserves = await pair.getReserves();
+      console.log("Pair address =", pairAddress[0], reserves[0]);
+    }
+}
+
+// get pair address by TokenA and TokenB
+const getPairAddress = async (tokenA: string, tokenB: string) => {
+    const factory = await getContract(factoryAddress);
+    const account = getAccount();
+    factory.connect(account);
+
+    const pairAddress = await factory.getPair(tokenA, tokenB);
+    console.log("Pair address =", pairAddress[0].toString());
+    return number.toHexString(pairAddress[0]);
+}
+
+const main = async () => {
+  // Testing Call code add 1_000_000
+  // mintToken(tokenAAddress, '1000000000000000000000000');
+  // mintToken(tokenBAddress, '1000000000000000000000000');
+  // mintToken(tokenCAddress, '1000000000000000000000000');
+  // mintToken(tokenDAddress, '1000000000000000000000000');
+
+  // createPair(tokenAAddress, tokenBAddress);
+  // createPair(tokenAAddress, tokenCAddress);
+  // createPair(tokenAAddress, tokenDAddress);
+  // createPair(tokenBAddress, tokenCAddress);
+  // createPair(tokenBAddress, tokenDAddress);
+  // createPair(tokenCAddress, tokenDAddress);
+
+  // getPairContractAddress(tokenAAddress, tokenBAddress);
+  // getPairContractAddress(tokenAAddress, tokenCAddress);
+  // getPairContractAddress(tokenAAddress, tokenDAddress);
+  // getPairContractAddress(tokenBAddress, tokenCAddress);
+  // getPairContractAddress(tokenBAddress, tokenDAddress);
+  // getPairContractAddress(tokenCAddress, tokenDAddress);
+
+  // approve(tokenAAddress, routerAddress, uint256.UINT_128_MAX.toString());
+  // approve(tokenBAddress, routerAddress, uint256.UINT_128_MAX.toString());
+  // approve(tokenCAddress, routerAddress, uint256.UINT_128_MAX.toString());
+  // approve(tokenDAddress, routerAddress, uint256.UINT_128_MAX.toString());
+
+  // checkAllowance(tokenAAddress, routerAddress);
+  // checkAllowance(tokenBAddress, routerAddress);
+  // checkAllowance(tokenCAddress, routerAddress);
+  // checkAllowance(tokenDAddress, routerAddress);
+
+  // addLiquidity(tokenAAddress, tokenBAddress, '50000000000000000000000');
+  // addLiquidity(tokenAAddress, tokenCAddress, '50000000000000000000000');
+  // addLiquidity(tokenAAddress, tokenDAddress, '50000000000000000000000');
+  // addLiquidity(tokenBAddress, tokenCAddress, '50000000000000000000000');
+  // addLiquidity(tokenBAddress, tokenDAddress, '50000000000000000000000');
+  // addLiquidity(tokenCAddress, tokenDAddress, '50000000000000000000000');
+
+  // listAllPairs();
+
+  // approveLiquidityAllowance(tokenAAddress, tokenDAddress, uint256.UINT_128_MAX.toString());
+  // removeLiquidity(tokenAAddress, tokenDAddress, '1000000000000000000');
+};
+main();
